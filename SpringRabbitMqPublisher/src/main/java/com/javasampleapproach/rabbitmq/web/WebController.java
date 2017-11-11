c6d8508414865394d42ae5d376ef6233406ee19d
manvendra80386 <manvendra80386@gmail.com>
2017-11-12 00:07:52 +0530

    Spring MQ Publisher and Subcriber

---
+++ SpringRabbitMqPublisher/src/main/java/com/javasampleapproach/rabbitmq/web/WebController.java
@@ -0,0 +1,21 @@
+package com.javasampleapproach.rabbitmq.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.javasampleapproach.rabbitmq.publisher.Publisher;
+
+@RestController
+public class WebController {
+	
+	@Autowired
+	Publisher publisher;
+	
+	@RequestMapping("/send")
+	public String sendMsg(@RequestParam("msg")String msg){
+		publisher.produceMsg(msg);
+		return "Done";
+	}
+}
